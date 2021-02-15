0e378a86eddaaffe8b892eb3ca22491be0bd3077
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-15 18:08:23 -0500

    [aboutRoutes] tbh I do not know but everyting is working

--- api/aboutRoutes.js
+++ api/aboutRoutes.js
@@ -21,7 +21,29 @@ router.get("/:id", async (req, res) => {
 // Update about content PUT
 router.put("/update", async (req, res) => {
     console.log(req.body);
-    res.send("you hit the route")
-})
+    const {
+        id,
+        name,
+        nameShow,
+        location,
+        locationShow,
+        work,
+        workShow,
+    } = req.body;
+
+    try {
+        const requestedAbout = await db.about.findOne({ where: { id: id } });
+        requestedAbout.name = name;
+        requestedAbout.nameShow = nameShow;
+        requestedAbout.location = location;
+        requestedAbout.locationShow = locationShow;
+        requestedAbout.work = work;
+        requestedAbout.workShow = workShow;
+        await requestedAbout.save();
+        res.status(200).json({ updatedAbout: requestedAbout });
+    } catch (error) {
+        res.status(400).json({ msg: "Couldn't update about", error: error })
+    }
+});
 
 module.exports = router;
