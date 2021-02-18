83f92d9c71597a456d3fdc9e0517086e06fe6b3c
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-18 16:19:09 -0500

    nothing really

--- migrations/20210217134050-remove_comment.js
+++ migrations/20210217134050-remove_comment.js
@@ -1,28 +1,26 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-    await queryInterface.removeColumn("links", "comment")
-  },
+    up: async (queryInterface, Sequelize) => {
+        /**
+         * Add altering commands here.
+         *
+         * Example:
+         * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+         */
+        await queryInterface.removeColumn("links", "comment");
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-    await queryInterface.addColumn("links", "comment",
-    {
-         allowNull: false,
-         type: Sequelize.BOOLEAN,
-     },
- );
-  }
+    down: async (queryInterface, Sequelize) => {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+         */
+        await queryInterface.addColumn("links", "comment", {
+            allowNull: false,
+            type: Sequelize.BOOLEAN,
+        });
+    },
 };
